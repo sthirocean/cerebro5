e702028a509a8ba7899657918d4a142f3da5f6b4
sthirocean <98005634+sthirocean@users.noreply.github.com>
2023-11-03 11:39:02 +0530

    Initial Commit for file: chat_data.js

---
+++ chat_data.js
@@ -0,0 +1,28 @@
+export const item = {
+    "chat_list": [
+        {
+            "contact_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/261.png",
+            "id": "43dfca2b-c31e-4869-a422-48f24cce8de0",
+            "contact_name": "person",
+            "last_message": "clear",
+            "timestamp": "person"
+        },
+        {
+            "contact_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/300.png",
+            "id": "392c498c-1757-4dc8-bc57-5dabf8f7ac0a",
+            "contact_name": "attorney",
+            "last_message": "attorney",
+            "timestamp": "throw"
+        },
+        {
+            "contact_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/417.png",
+            "id": "e1e9ed3b-c186-4838-862b-98d5395432b7",
+            "contact_name": "throw",
+            "last_message": "oil",
+            "timestamp": "tax"
+        }
+    ],
+    "id": "75ab436f-d23c-4cc3-90ef-161568a43332",
+    "plus": true,
+    "search": true
+}
